1b9d566ed168e40c32e4df6fe3ba65b02ca35e74
Vivian Lin <linvivian7@gmail.com>
2018-04-15 14:03:27 -0700

    Add beaufort

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
 import { reducer as form } from 'redux-form';
 
+import beaufort from 'beaufort-scale';
 import { parseQueryResponse } from '../utils/query';
 import { SAVE_TIMEZONE, SAVE_WEATHER } from '../actions';
 
@@ -19,6 +20,11 @@ const weatherInfo = (state = {}, {type, payload}) => {
 
     if (type === SAVE_WEATHER) {
         newState = parseQueryResponse(payload);
+
+        let beaufortWindScore = beaufort(newState.wind.speed);
+        console.log(beaufort(1000000));
+
+        debugger;
     }
     return newState;
 };
