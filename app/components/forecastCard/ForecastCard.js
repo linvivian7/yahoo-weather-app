789e3af7d8efda3f9cd2287c1cc3c7448ad3e870
Vivian Lin <linvivian7@gmail.com>
2018-04-17 09:48:47 -0700

    Start styling ForecastCards

--- app/components/forecastCard/ForecastCard.js
+++ app/components/forecastCard/ForecastCard.js
@@ -1,20 +1,42 @@
 import React from 'react';
+import classNames from 'classnames';
 
 import './forecastCard.scss';
 
 const ForecastCard = ({ item, index }) => {
     const {
+        code,
         date,
         high,
         low
     } = item;
     const dayOfWeek = item.day;
     const [day, month] = date.split(' ');
+    const forecastCardClasses = classNames(
+        'forecast-container',
+        `forecast-container-${index}`
+    );
+    const weatherIconClasses = classNames(
+        'wi',
+        `wi-yahoo-${code}`
+    );
 
     return (
-        <div className={ `forecast-container-${index}` }>
-            <div className="location-name">
-                { `${dayOfWeek} ${month} ${day} ${high}`}<i className="wi wi-celsius"></i> { `${low}` } <i className="wi wi-celsius"></i>
+        <div className={ forecastCardClasses }>
+            <div className="dayOfWeek">
+                { `${dayOfWeek} `}
+            </div>
+            <div className="weather-icon">
+                <i className={ weatherIconClasses }></i>
+            </div>
+            <div className="temperature-hi">
+                { high }<i className="wi wi-celsius"></i>
+            </div>
+            <div className="temperature-low">
+                { low }<i className="wi wi-celsius"></i>
+            </div>
+            <div className="date">
+                <div>{ `${ month } ${ day } `}</div>
             </div>
         </div>
     );
